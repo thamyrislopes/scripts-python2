da3f6f2d39e0324688b861efb9bc5642280e36cc
Perkles <thamyrisloopes@gmail.com>
2025-05-07 06:18:17 -0300

    Update ex060.py

--- ex060.py
+++ ex060.py
@@ -1,3 +1,11 @@
 '''Faça um programa que leia um número qualquer e mostre o fatorial dele.
 
 Ex: 5! = 5x4x3x2x1 = 120'''
+c = 1
+n = int(input('Digite um número inteiro: '))
+while n > 0:
+    if n != 0:
+        print('{}x'.format(n), end='')
+        n -= c
+    
+    
