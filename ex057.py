695787225f34b9b69f084cad4a3474947b24146b
Perkles <thamyrisloopes@gmail.com>
2025-05-10 14:48:22 -0300

    update

--- ex057.py
+++ ex057.py
@@ -1,5 +1,11 @@
 #Faça um programa que leia o sexo, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.
-s = 'F'
+
+'''s = 'F'
 while s == 'F' or s == 'M':
     s = str(input('O sexo é feminino ou masculino? Escolha uma das opções [F/M]: ')).upper()
-print('Escolha uma opção válida!')
+print('Escolha uma opção válida!')'''
+
+sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper() [0]
+while sexo not in 'MmFf':
+    sexo = str(input('Dados inválidos. Por favor, informe seu sexo: ')).strip().upper()[0]
+print('Sexo {} registrado com sucesso.'.format(sexo))
--- ex060.py
+++ ex060.py
@@ -2,10 +2,15 @@
 
 Ex: 5! = 5x4x3x2x1 = 120'''
 c = 1
+n = 2 
+m = n - 1
 n = int(input('Digite um número inteiro: '))
+print('{}! = '.format(n), end='')
 while n > 0:
     if n != 0:
         print('{}x'.format(n), end='')
+        m = m * n
         n -= c
+print(' = {}'.format(m))
     
     
