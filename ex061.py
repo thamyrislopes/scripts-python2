2df868b6463ef68742882bf591526ca1f743707d
Perkles <thamyrisloopes@gmail.com>
2025-05-26 20:55:23 -0300

    update

--- ex061.py
+++ ex061.py
@@ -1,6 +1,6 @@
 '''Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão, usando a estrutura 'while'''
 
-print('='*30)
+'''print('='*30)
 print('10 TERMOS DE UMA P.A.'.center(30))
 print('='*30)
 primeiro = int(input('Primeiro termo: '))
@@ -9,4 +9,17 @@ décimo = primeiro + 10 * razão
 while primeiro != décimo:
     print('{}'.format(primeiro), end=' → ')
     primeiro += razão
+print('FIM.')'''
+
+print('-=' * 10)
+print('Gerados de P.A.')
+print('-=' * 10)
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão da P.A: '))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print('{} → '.format(termo), end='')
+    termo += razão
+    cont += 1
 print('FIM.')
--- ex062.py
+++ ex062.py
@@ -1,14 +1,21 @@
 '''Melhore o DESAFIO 061, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerra quando ele disser que quer mostrar 0 termos.'''
 
-print('='*30)
-print('10 TERMOS DE UMA P.A.'.center(30))
-print('='*30)
+print('-=' * 10)
+print('Gerados de P.A.')
+print('-=' * 10)
 primeiro = int(input('Primeiro termo: '))
-razão = int(input('Razão: '))
-décimo = primeiro + 10 * razão
-resposta = 'Ss'
-while primeiro != décimo and resposta == 'Ss':
-    print('{}'.format(primeiro), end=' → ')
-    primeiro += razão
-print(end='\n')
+razão = int(input('Razão da P.A: '))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total += mais
+    while cont <= total:
+        print('{} → '.format(termo), end='')
+        termo += razão
+        cont += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+print('Progressão finalizada com {} termos mostrados.'.format(total))
 
